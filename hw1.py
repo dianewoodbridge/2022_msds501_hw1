41d760d0ae03fc5ef3d13f06e91a18cb30a095da
Diane Woodbridge <dwoodbriege@usfca.edu>
2022-07-07 16:26:55 -0700

    added hw1.py

---
+++ hw1.py
@@ -0,0 +1,9 @@
+print("-----------------")
+for i in range(0, 10):
+    print("==================")
+    print(f'''interation : {i}''')
+    if(i % 2 != 0):
+        print("This is an odd number")
+       
+print("-----------------")
+print("end")
