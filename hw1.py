6d8da071e074b59d7bc958f301ea359c204eb877
Diane Woodbridge <dwoodbriege@usfca.edu>
2022-07-07 21:06:01 -0700

    fixed typo

--- hw1.py
+++ hw1.py
@@ -1,7 +1,7 @@
 print("-----------------")
 for i in range(0, 10):
     print("==================")
-    print(f'''interation : {i}''')
+    print(f'''iteration  : {i}''')
     if(i % 2 != 0):
         print("This is an odd number")
        
